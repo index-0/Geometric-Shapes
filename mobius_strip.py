2597e8d9a37df7fe07aca27a980c330be3940510
Alfredo Perez <alfredoperez1998@gmail.com>
2017-05-19 23:56:51 -0500

    Mobius Strip

---
+++ mobius_strip.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri as mtri
+
+fig = plt.figure(figsize=plt.figaspect(1))
+
+u = np.linspace(0, 2.0 * np.pi, endpoint=True, num=20)
+v = np.linspace(-0.5 , 0.5, endpoint=True, num=10)
+u, v = np.meshgrid(u, v)
+u, v = u.flatten(), v.flatten()
+
+x = (1 + 0.5 * v * np.cos(u / 2.0)) * np.cos(u)
+y = (1 + 0.5 * v * np.cos(u / 2.0)) * np.sin(u)
+z = 0.5 * v * np.sin(u / 2.0)
+
+tri = mtri.Triangulation(u, v)
+
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(x, y, z, triangles=tri.triangles)
+ax.set_title('Banda de Mobius')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ax.set_zlim(-1, 1)
+
+
+plt.show()
