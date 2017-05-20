65601fb51023c6178f35f01075e81064cc9bd28d
Alfredo Perez <alfredoperez1998@gmail.com>
2017-05-20 02:46:36 -0500

    Umbilic torus

---
+++ umbilic_torus.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri as mtri
+
+fig = plt.figure(figsize=plt.figaspect(1))
+
+u = np.linspace(-np.pi, np.pi, endpoint=True, num=50)
+v = np.linspace(-np.pi, np.pi, endpoint=True, num=50)
+u, v = np.meshgrid(u, v)
+u, v = u.flatten(), v.flatten()
+
+x = np.sin(u) * (7 + np.cos(u / 3 - 2 * v) + 2 * np.cos(u / 3 + v))
+y = np.cos(u) * (7 + np.cos(u / 3 - 2 * v) + 2 * np.cos(u / 3 + v))
+z = np.sin(u / 3 - 2 * v) + 2 * np.sin(u / 3 + v)
+
+tri = mtri.Triangulation(u, v)
+
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(x, y, z, triangles=tri.triangles)
+ax.set_title('Umbilic torus')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+
+plt.show()
