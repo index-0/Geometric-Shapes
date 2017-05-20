c855ad681e405b7f796ac495e02280fc25075bf7
Alfredo Perez <alfredoperez1998@gmail.com>
2017-05-20 00:49:47 -0500

    Sphere

---
+++ sphere.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri as mtri
+
+fig = plt.figure(figsize=plt.figaspect(1))
+
+u = np.linspace(0, 2.0 * np.pi, endpoint=True, num=25)
+v = np.linspace(0 , np.pi, endpoint=True, num=25)
+u, v = np.meshgrid(u, v)
+u, v = u.flatten(), v.flatten()
+
+x = np.cos(u)*np.sin(v)
+y = np.sin(u)*np.sin(v)
+z = np.cos(v)
+
+tri = mtri.Triangulation(u, v)
+
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(x, y, z, triangles=tri.triangles)
+ax.set_title('Sphere')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ax.set_zlim(-1, 1)
+
+plt.show()
